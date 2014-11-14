d18352de4c127b1cc0530b257b2bfa410a3689fa
Kai Huang <khuang29@illinois.edu>
2014-11-13 22:15:16 -0600

    initial commit

---
+++ appopinion/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from appopinion.models import *
+
+admin.site.register(Profile)
---
+++ appopinion/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.ForeignKey(User)
+    motto = models.CharField(max_length=200)
