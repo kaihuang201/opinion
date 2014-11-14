1d6763d0bdd1745cf1472e12c4bf5ff29396c359
Kai Huang <khuang29@illinois.edu>
2014-11-14 01:03:35 -0600

    added git ignore, untracked pyc and swp and ~files

--- opinion/settings.py
+++ opinion/settings.py
@@ -59,12 +59,8 @@ WSGI_APPLICATION = 'opinion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'opinion',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
