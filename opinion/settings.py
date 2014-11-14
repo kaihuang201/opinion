192e57a32767cf288cc54170b592006bcf503a9b
Kai Huang <kaihuang201@users.noreply.github.com>
2014-11-14 01:14:35 -0600

    Update settings.py

--- opinion/settings.py
+++ opinion/settings.py
@@ -59,10 +59,23 @@ WSGI_APPLICATION = 'opinion.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'opinion',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
