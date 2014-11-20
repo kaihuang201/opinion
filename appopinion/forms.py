2f5320aacb92485aea8a9a1f84d64ea3cd8be6ae
Kai Huang <khuang29@illinois.edu>
2014-11-20 13:53:59 -0600

    added default text for account forms

--- appopinion/forms.py
+++ appopinion/forms.py
@@ -4,17 +4,25 @@ class signinForm(forms.Form):
 	username = forms.CharField(
                 max_length=30, 
 	            widget=forms.TextInput(attrs={
-                                'class' : 'form-control'}),
+                                'class' : 'form-control',
+                                'placeholder' : 'Username',
+                            }),
                 )
 
 	password = forms.CharField(max_length=30, 
 	            widget=forms.PasswordInput(attrs={
-                                'class' : 'form-control'}))
+                                'class' : 'form-control',
+                                'placeholder' : 'Password',
+                            }))
 
 class signupForm(signinForm):
 	password_again = forms.CharField(max_length=30, 
-	            widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+	            widget=forms.PasswordInput(attrs={'class' : 'form-control',
+                                'placeholder' : 'Password Again',
+                            }))
 
 class profileEditForm(forms.Form):
      motto = forms.CharField(max_length=200,
-                widget=forms.TextInput(attrs={'class':'form-control'}))
+                widget=forms.TextInput(attrs={'class':'form-control',
+                                'placeholder' : 'Enter Your Motto',
+                            }))
